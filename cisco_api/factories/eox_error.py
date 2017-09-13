1f89dca84624ad611f820363db2cfb835a1cf983
Glen Harmon <glencharmon@gmail.com>
2017-09-13 22:18:03 +0200

    exceptions

--- cisco_api/factories/eox_error.py
+++ cisco_api/factories/eox_error.py
@@ -1,8 +1,50 @@
 
-from cisco_api.utilities.exceptions import InvalidProduct
+from cisco_api.utilities.exceptions import (InvalidProduct,
+                                            GeneralException,
+                                            InvalidStartDate,
+                                            SoftwareReleaseTooLong,
+                                            TooManySerialNumbers,
+                                            TooManyProductIds,
+                                            InvalidResponseEncoding,
+                                            Unavailable,
+                                            ServiceProblem,
+                                            ProductIdNotFound,
+                                            SerialToProductIdServiceDown,
+                                            ProductBulletinDoesNotExist,
+                                            SerialDoesNotExist,
+                                            SoftwareDoesNotExist,
+                                            ProductIdDoesNotExist,
+                                            InvalidPageIndex,
+                                            ProductBulletinNotFound,
+                                            RequestNotUtf8Compliant,
+                                            InvalidProductIdWildcard,
+                                            AaaServiceUnavailable,
+                                            AccessDenied,
+                                            CepmServiceUnavailable)
 
 EOX_EXCEPTIONS = [
     InvalidProduct,
+    GeneralException,
+    InvalidStartDate,
+    SoftwareReleaseTooLong,
+    TooManySerialNumbers,
+    TooManyProductIds,
+    InvalidResponseEncoding,
+    Unavailable,
+    ServiceProblem,
+    ProductIdNotFound,
+    SerialToProductIdServiceDown,
+    ProductBulletinDoesNotExist,
+    SerialDoesNotExist,
+    SoftwareDoesNotExist,
+    ProductIdDoesNotExist,
+    InvalidPageIndex,
+    ProductBulletinNotFound,
+    RequestNotUtf8Compliant,
+    InvalidProductIdWildcard,
+    AaaServiceUnavailable,
+    AccessDenied,
+    CepmServiceUnavailable,
 ]
 
 class EoxErrorFactory:
@@ -16,5 +58,8 @@ class EoxErrorFactory:
             description = record['EOXError']['ErrorDescription']
             error_id = record['EOXError']['ErrorID']
             for eox_exception in EOX_EXCEPTIONS:
-                if error_id == eox_exception.error_id:
+                error_ids = eox_exception.error_id
+                if not isinstance(eox_exception.error_id, list):
+                    error_ids = [eox_exception.error_id]
+                if error_id in error_ids:
                     raise eox_exception(description)
--- cisco_api/utilities/exceptions.py
+++ cisco_api/utilities/exceptions.py
@@ -1,3 +1,98 @@
 
+
 class InvalidProduct(Exception):
     error_id = 'SSA_ERR_026'
+
+
+class GeneralException(Exception):
+    error_id = 'SSA_GENERIC_ERR'
+
+
+class InvalidStartDate(Exception):
+    error_id = [
+        'SSA_ERR_001',
+        'SSA_ERR_016',
+    ]
+
+
+class SoftwareReleaseTooLong(Exception):
+    error_id = 'SSA_ERR_003'
+
+
+class TooManySerialNumbers(Exception):
+    error_id = 'SSA_ERR_007'
+
+
+class TooManyProductIds(Exception):
+    error_id = 'SSA_ERR_009'
+
+
+class InvalidResponseEncoding(Exception):
+    error_id = 'SSA_ERR_010'
+
+
+class Unavailable(Exception):
+    error_id = [
+        'SSA_ERR_011',
+        'SSA_ERR_012',
+        'SSA_ERR_014',
+    ]
+
+
+class ServiceProblem(Exception):
+    error_id = 'SSA_ERR_013'
+
+
+class ProductIdNotFound(Exception):
+    error_id = 'SSA_ERR_015'
+
+
+class SerialToProductIdServiceDown(Exception):
+    error_id = 'SSA_ERR_018'
+
+
+class ProductBulletinDoesNotExist(Exception):
+    error_id = 'SSA_ERR_022'
+
+
+class SerialDoesNotExist(Exception):
+    error_id = 'SSA_ERR_023'
+
+
+class SoftwareDoesNotExist(Exception):
+    error_id = 'SSA_ERR_024'
+
+
+class ProductIdDoesNotExist(Exception):
+    error_id = 'SSA_ERR_026'
+
+
+class InvalidPageIndex(Exception):
+    error_id = 'SSA_ERR_028'
+
+
+class ProductBulletinNotFound(Exception):
+    error_id = 'SSA_ERR_030'
+
+
+class RequestNotUtf8Compliant(Exception):
+    error_id = 'SSA_ERR_031'
+
+
+class InvalidProductIdWildcard(Exception):
+    error_id = 'SSA_ERR_032'
+
+
+class AaaServiceUnavailable(Exception):
+    error_id = 'SSA_ERR_033'
+
+
+class AccessDenied(Exception):
+    error_id = [
+        'SSA_ERR_034',
+        'SSA_ERR_036',
+    ]
+
+
+class CepmServiceUnavailable(Exception):
+    error_id = 'SSA_ERR_037'
